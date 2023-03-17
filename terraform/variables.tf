adf8fe13566562c2e6c3aaf90a1ffd481f7576b9
Harsha Jain <harshajain@192.168.1.168>
2023-03-17 10:45:12 +0530

    update 17-March-23 10:45AM

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,24 +1,19 @@
 variable "root_ec2_ami" {
     type = string 
-    default = "ami-0f8ca728008ff5af4"
 }
 
 variable "root_ec2_type" {
     type = string 
-    default = "t2.micro"
 }
 
 variable "root_sg_name" {
     type = string 
-    default = "my_sg"
 }
 
 variable "root_key_name" {
     type = string 
-    default = "my_pem"
 }
 
 variable "root_key_path" {
     type = string 
-    default = "./keys/my_pem.pem"
 }
