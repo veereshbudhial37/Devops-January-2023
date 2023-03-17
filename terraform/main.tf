8c8b00a26321cf99176ecdfef6e669aeff1897ca
Harsha Jain <harshajain@192.168.1.168>
2023-03-17 09:58:46 +0530

    module 17-March-23 09:58AM

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,65 +1,21 @@
-locals {
-    app_port = 80
+module "create_sg" {
+    source = "./modules/create_sg"
+    sg_name = var.root_sg_name
 }
 
-resource "tls_private_key" "rsa" {
-   algorithm = "RSA"
-   rsa_bits = 4096
+module "create_pem" {
+    source = "./modules/create_pem"
+    key_name = var.root_key_name
+    key_path = var.root_key_path
 }
 
-resource "aws_key_pair" "pem_file" {
-    key_name = var.key_name
-    public_key = tls_private_key.rsa.public_key_openssh
+module "create_ec2_2" {
+    source = "github.com/jaintpharsha/terra_module.git//create_ec2"
+    ami_id = var.root_ec2_ami
+    ec2_type = var.root_ec2_type
+    ec2_pem = module.create_pem.ec2_pem
+    ec2_sg_id = module.create_sg.sg_id
 }
 
-resource "local_file" "pem-key" {
-    content = tls_private_key.rsa.private_key_pem
-    filename = var.key_path
-}
 
-resource "aws_security_group" "allow_ssh" {
-  name        = var.sg_name
-  description = "Allow SSH inbound traffic"
 
-  ingress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port        = local.app_port
-    to_port          = local.app_port
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_tls"
-  }
-}
-
-resource "aws_instance" "my_ec2_instance" {
-    ami = var.ami_id
-    instance_type = var.ec2_type
-    key_name = aws_key_pair.pem_file.key_name
-    vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-    tags = {
-        Name = "Terraform"
-    }
-}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,24 +1,24 @@
-variable "ec2_type" {
-    type = string
-    default = "t2.micro"
-}
-
-variable "ami_id" {
-    type = string
+variable "root_ec2_ami" {
+    type = string 
     default = "ami-0f8ca728008ff5af4"
 }
 
-variable "key_name" {
-    type = string
+variable "root_ec2_type" {
+    type = string 
+    default = "t2.micro"
+}
+
+variable "root_sg_name" {
+    type = string 
+    default = "my_sg"
+}
+
+variable "root_key_name" {
+    type = string 
     default = "my_pem"
 }
 
-variable "key_path" {
-    type = string
+variable "root_key_path" {
+    type = string 
     default = "./keys/my_pem.pem"
-}
-
-variable "sg_name" {
-    type = string
-    default = "my_sg"
 }
