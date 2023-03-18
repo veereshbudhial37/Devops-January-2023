00d65daa4759ec3c8914140ff9e81138f4fbf839
Harsha Jain <harshajain@192.168.1.168>
2023-03-18 10:45:57 +0530

    update 18-March-23 10:45AM

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -17,3 +17,27 @@ variable "root_key_name" {
 variable "root_key_path" {
     type = string 
 }
+
+variable "root_s3_backend_name" {
+    type = string 
+}
+
+variable "root_dynamodb_name" {
+    type = string 
+}
+
+variable "root_dynamodb_key" {
+    type = string 
+}
+
+variable "root_ec2_user" {
+    type = string 
+}
+
+variable "root_source_path" {
+    type = string 
+}
+
+variable "root_destination_path" {
+    type = string 
+}
