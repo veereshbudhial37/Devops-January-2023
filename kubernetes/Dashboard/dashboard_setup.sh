ec84c190a95fac958f20d52fc70c95acfb6773f9
Harsha Jain <jaintpharsha@gmail.com>
2023-04-03 08:41:16 +0530

    pod 03-April-23 08:41AM

--- kubernetes/Dashboard/dashboard_setup.sh
+++ kubernetes/Dashboard/dashboard_setup.sh
@@ -19,3 +19,6 @@ rm nodeport_dashboard_patch.yaml
 echo -e "\n   DASHBOARD_ENDPOINT: Shttps://<any_worker_node_ip>:32000"
 echo -e "\n   USE BELLOW TOKEN TO LOGIN K8S_DASHBOARD\n"
 kubectl describe secret -n kubernetes-dashboard kubernetes-dashboard-token | grep -i 'token:      ' | awk -F 'token:      ' '{print $NF}'
+
+
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/tigera-operator.yaml
